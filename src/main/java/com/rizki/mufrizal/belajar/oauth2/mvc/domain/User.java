2e33c7aa4d1a58d2496d222a3abc21b195b68849
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-03 21:38:14 +0700

    Add User Domain

---
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/domain/User.java
@@ -0,0 +1,73 @@
+package com.rizki.mufrizal.belajar.oauth2.mvc.domain;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import org.hibernate.annotations.*;
+import org.hibernate.annotations.CascadeType;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by rizki on 03/02/15.
+ */
+
+@Entity
+@Table(name = "tb_user",
+    indexes = {
+            @Index(columnList = "username", name = "username")
+    }
+)
+@JsonAutoDetect
+public class User implements Serializable{
+    
+    @Id
+    @Column(name = "username", length = 30, unique = true)
+    private String username;
+    
+    @Column(name = "password", length = 200)
+    private String password;
+    
+    @Column(name = "enable", columnDefinition = "TINYINT")
+    private boolean enable;
+    
+    @OneToMany(fetch = FetchType.LAZY)
+    @Cascade({CascadeType.ALL})
+    private Set<UserRole> userRoles = new HashSet<UserRole>();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+}
