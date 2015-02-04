c90dc25209b0cebab8db112633ef1fa2f0f0858c
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-04 09:17:41 +0700

    Add Web Security Config With password encoder and Annotation Secure

--- src/main/java/com/rizki/mufrizal/belajar/oauth2/config/WebSecurityConfig.java
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/config/WebSecurityConfig.java
@@ -1,7 +1,47 @@
 package com.rizki.mufrizal.belajar.oauth2.config;
 
+import com.rizki.mufrizal.belajar.oauth2.mvc.service.UserDetailLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 /**
  * Created by rizki on 03/02/15.
  */
-public class WebSecurityConfig {
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableWebMvcSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailLoginService userDetailLoginService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userDetailLoginService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
