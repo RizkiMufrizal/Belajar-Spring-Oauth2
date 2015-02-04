bbf337aa951574ee6c3c1b2c32c5b4ad259197a5
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-04 09:13:22 +0700

    Add Query for Login

--- src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/repository/UserRepository.java
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.rizki.mufrizal.belajar.oauth2.mvc.repository;
 
 import com.rizki.mufrizal.belajar.oauth2.mvc.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,5 +11,7 @@ import org.springframework.stereotype.Repository;
  */
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, String> {
+    @Query("select u from User u left join fetch u.userRoles pd where u.username = :username")
+    public User getUserByUsername(@Param("username") String username);
 }
