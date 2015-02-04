11f4b623683530d26cf4b9ea7c354fd16f769efb
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-04 11:34:13 +0700

    Optimize Import

--- src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/controller/HelloController.java
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/controller/HelloController.java
@@ -1,13 +1,9 @@
 package com.rizki.mufrizal.belajar.oauth2.mvc.controller;
 
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by rizki on 03/02/15.
