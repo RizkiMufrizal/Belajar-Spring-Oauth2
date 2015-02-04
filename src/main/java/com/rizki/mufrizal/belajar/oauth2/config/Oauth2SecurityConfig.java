00308f2169fb435d1754f9bee4525d73a3d44a94
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-04 09:17:05 +0700

    Add Oauth2 Config

---
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/config/Oauth2SecurityConfig.java
@@ -0,0 +1,91 @@
+package com.rizki.mufrizal.belajar.oauth2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+
+/**
+ * Created by rizki on 04/02/15.
+ */
+
+@Configuration
+public class Oauth2SecurityConfig {
+
+    private static final String RESOURCE_ID = "belajar";
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resourceServerSecurityConfigurer) {
+            resourceServerSecurityConfigurer
+                    .resourceId(RESOURCE_ID);
+        }
+
+        @Override
+        public void configure(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity
+                    .authorizeRequests()
+                    .antMatchers("/", "/hello").permitAll()
+                    .and()
+                    .addFilterBefore(new CORSFilter(), ChannelProcessingFilter.class);
+        }
+
+    }
+
+    @Configuration
+    @EnableResourceServer
+    private static class AuhorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+        private TokenStore tokenStore = new InMemoryTokenStore();
+
+        @Autowired
+        @Qualifier("authenticationManagerBean")
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer authorizationServerEndpointsConfigurer) throws Exception {
+            authorizationServerEndpointsConfigurer
+                    .tokenStore(this.tokenStore)
+                    .authenticationManager(this.authenticationManager);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clientDetailsServiceConfigurer) throws Exception {
+            clientDetailsServiceConfigurer
+                    .inMemory()
+                    .withClient("clientapp")
+                    .authorizedGrantTypes("password", "refresh_token")
+                    .authorities("USER")
+                    .scopes("read", "write")
+                    .resourceIds(RESOURCE_ID)
+                    .secret("123456");
+        }
+
+        @Bean
+        @Primary
+        public DefaultTokenServices defaultTokenServices() {
+            DefaultTokenServices tokenServices = new DefaultTokenServices();
+            tokenServices.setSupportRefreshToken(true);
+            tokenServices.setTokenStore(this.tokenStore);
+            return tokenServices;
+        }
+
+    }
+
+}
