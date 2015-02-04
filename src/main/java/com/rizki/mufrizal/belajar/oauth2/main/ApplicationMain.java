ed35eb9333c4e654f2c2abe0296f030235a9ef3c
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-04 18:24:29 +0700

    remove springBootApplication

--- src/main/java/com/rizki/mufrizal/belajar/oauth2/main/ApplicationMain.java
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/main/ApplicationMain.java
@@ -2,13 +2,11 @@ package com.rizki.mufrizal.belajar.oauth2.main;
 
 import com.rizki.mufrizal.belajar.oauth2.config.WebConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by rizki on 03/02/15.
  */
 
-@SpringBootApplication
 public class ApplicationMain {
 
     public static void main(String[] args) {
