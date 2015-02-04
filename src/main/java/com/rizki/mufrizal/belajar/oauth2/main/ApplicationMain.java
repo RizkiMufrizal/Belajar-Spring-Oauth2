f00f396f6a318afc25bf32f4bdead8b029430e84
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-04 09:15:12 +0700

    format Code

--- src/main/java/com/rizki/mufrizal/belajar/oauth2/main/ApplicationMain.java
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/main/ApplicationMain.java
@@ -10,9 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApplicationMain {
-    
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         SpringApplication.run(WebConfig.class);
     }
-    
+
 }
