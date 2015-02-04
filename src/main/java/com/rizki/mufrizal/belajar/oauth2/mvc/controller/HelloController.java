ddb97283e9a6af9a501566ec7144174ca71e5eea
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-04 11:32:39 +0700

    add User page

--- src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/controller/HelloController.java
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/controller/HelloController.java
@@ -1,9 +1,13 @@
 package com.rizki.mufrizal.belajar.oauth2.mvc.controller;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by rizki on 03/02/15.
@@ -21,6 +25,12 @@ public class HelloController {
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String admin() {
         return "admin page";
+    }
+
+    @Secured({"ROLE_USER"})
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String user() {
+        return "user page";
     }
 
 }
