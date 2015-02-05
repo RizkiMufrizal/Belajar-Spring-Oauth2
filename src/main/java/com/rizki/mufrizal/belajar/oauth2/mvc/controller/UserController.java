f1bdb2a1a80859d993bb6fed6b484c6fb9b74ba3
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-05 07:26:21 +0700

    Add User Controller

---
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.rizki.mufrizal.belajar.oauth2.mvc.controller;
+
+import com.rizki.mufrizal.belajar.oauth2.mvc.domain.User;
+import com.rizki.mufrizal.belajar.oauth2.mvc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by rizki on 05/02/15.
+ */
+
+@RestController
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+    
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public List<User> getUserList(){
+        return userService.getUserList();
+    }
+    
+    @Secured({"ROLE_USER"})
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public void saveUser(@RequestBody User user){
+        userService.save(user);
+    }
+    
+}
