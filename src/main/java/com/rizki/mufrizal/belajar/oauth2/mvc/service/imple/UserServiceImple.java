a7dbed343a47f76eb2f27f325c921aabb3d99ddf
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-05 07:26:06 +0700

    Add User Service Implement

---
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/service/imple/UserServiceImple.java
@@ -0,0 +1,33 @@
+package com.rizki.mufrizal.belajar.oauth2.mvc.service.imple;
+
+import com.rizki.mufrizal.belajar.oauth2.mvc.domain.User;
+import com.rizki.mufrizal.belajar.oauth2.mvc.repository.UserRepository;
+import com.rizki.mufrizal.belajar.oauth2.mvc.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by rizki on 05/02/15.
+ */
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImple implements UserService {
+    
+    @Resource
+    private UserRepository userRepository;
+    
+    @Transactional(readOnly = false)
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getUserList() {
+        return userRepository.findAll();
+    }
+}
