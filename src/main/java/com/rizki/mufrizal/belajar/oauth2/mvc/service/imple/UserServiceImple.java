72c4729536499c3a96e31e95c11569f63234e516
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-05 07:37:20 +0700

    Update User Service Save

--- src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/service/imple/UserServiceImple.java
+++ src/main/java/com/rizki/mufrizal/belajar/oauth2/mvc/service/imple/UserServiceImple.java
@@ -1,13 +1,17 @@
 package com.rizki.mufrizal.belajar.oauth2.mvc.service.imple;
 
 import com.rizki.mufrizal.belajar.oauth2.mvc.domain.User;
+import com.rizki.mufrizal.belajar.oauth2.mvc.domain.UserRole;
 import com.rizki.mufrizal.belajar.oauth2.mvc.repository.UserRepository;
 import com.rizki.mufrizal.belajar.oauth2.mvc.service.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by rizki on 05/02/15.
@@ -23,6 +27,17 @@ public class UserServiceImple implements UserService {
     @Transactional(readOnly = false)
     @Override
     public void save(User user) {
+        
+        //enkripsi password
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
+        UserRole userRole = new UserRole();
+        userRole.setUser(user);
+        Set<UserRole> userRoles = new HashSet<UserRole>();
+        userRoles.add(userRole);
+        user.setUserRoles(userRoles);
+
         userRepository.save(user);
     }
 
